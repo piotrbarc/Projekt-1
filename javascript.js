34b61f6eb0a67c716a90cb9b3b8beaba437266f2
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-02-16 10:49:04 +0100

    dodanie kolorów

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
 let canvas = document.querySelector(`#canvas`)
 let ctx = canvas.getContext(`2d`);
+// adding image
 const image = document.querySelector(`#source`);
 ctx.drawImage(image, 0,0 , 698, 298);
 
 
 
+// draw in canvas
+
 let mouse = {x: 0, y: 0};
- 
 
 
 ctx.lineWidth = 3;
@@ -18,7 +20,7 @@ canvas.addEventListener(`mousemove`, function(e) {
     mouse.x = e.pageX - this.offsetLeft;
     mouse.y = e.pageY - this.offsetTop;
   }, false);
-canvas.addEventListener(`mousedown`, (e) => {
+canvas.addEventListener(`mousedown`, (e) => {e
     ctx.beginPath();
     ctx.moveTo(mouse.x, mouse.y);
  
@@ -33,13 +35,40 @@ let onPaint = () => {
     ctx.lineTo(mouse.x, mouse.y);
     ctx.stroke();
 };
-
-
-
-
-
-
 newFunction = () => {
     return `mousemove`;
 }
+
+//change color
+
+
+
+const red = document.querySelector(`#red`);
+const green = document.querySelector(`#green`);
+const yellow = document.querySelector(`#yellow`);
+const black = document.querySelector(`#black`);
+
+red.addEventListener(`click`, () => {
+
+    ctx.strokeStyle = "#FF0000";
+
+})
+
+green.addEventListener(`click`, () => {
+
+    ctx.strokeStyle = "#00FF7F";
+
+})
+
+black.addEventListener(`click`, () => {
+
+    ctx.strokeStyle = `#000000`;
+
+})
+
+yellow.addEventListener(`click`, () => {
+
+    ctx.strokeStyle = "#FFFF00";
+
+})
 
